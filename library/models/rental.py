361f52cc39cec8c06092de0dafa3d3c0d05793ec
Stanislas Sobieski <sts@odoo.com>
2018-09-10 15:27:38 +0200

    [SOLUTION] 02 - Fields

--- library/models/rental.py
+++ library/models/rental.py
@@ -9,5 +9,12 @@ class Rental(models.Model):
     customer_id = fields.Many2one('library.partner', string='Customer')
     book_id = fields.Many2one('library.book', string='Book')
 
-    rental_date = fields.Date()
-    return_date = fields.Date()
+    rental_date = fields.Date(string='Rental date', default=fields.Date.context_today)
+    return_date = fields.Date(string='Return date')
+
+    customer_address = fields.Text(related='customer_id.address')
+    customer_email = fields.Char(related='customer_id.email')
+
+    book_authors = fields.Many2many(related='book_id.author_ids')
+    book_edition_date = fields.Date(related='book_id.edition_date')
+    book_publisher = fields.Many2one(related='book_id.publisher_id')
