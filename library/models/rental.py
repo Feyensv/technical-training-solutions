7bc7bf45e9d813fa9b8fc19aa4717e49399bd377
Victor Feyens <vfe@odoo.com>
2019-03-20 14:48:54 +0100

    [SOLUTION] 01 - Models : Ex 2

--- library/models/rental.py
+++ library/models/rental.py
@@ -6,8 +6,11 @@ class Rental(models.Model):
     _name = 'library.rental'
     _description = 'Book rental'
 
-    customer_id = fields.Many2one('library.partner', string='Customer')
-    book_id = fields.Many2one('library.book', string='Book')
-
-    rental_date = fields.Date()
-    return_date = fields.Date()
+    customer_id = fields.Many2one(
+        'library.partner',
+        string='Customer')
+    book_id = fields.Many2one(
+        'library.book',
+        string='Book')
+    rental_date = fields.Date(string='Rental date')
+    return_date = fields.Date(string='Return date')
